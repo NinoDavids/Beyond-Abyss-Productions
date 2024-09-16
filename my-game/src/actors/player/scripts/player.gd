10de1569e6ed38e26b45014cedb373999e92d97a
NinoDavids <60606129+NinoDavids@users.noreply.github.com>
2024-09-16 11:37:26 +0200

    Added pickup and drop functions

--- my-game/src/actors/player/scripts/player.gd
+++ my-game/src/actors/player/scripts/player.gd
@@ -6,6 +6,9 @@ class_name Player
 const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
 
+var objectHeld = false
+var heldObject: Node3D = null
+
 @export var mouse_sens: float = 0.25
 
 func _ready() -> void:
@@ -13,15 +16,55 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("quitEditor")):
-		get_tree().quit();
+		get_tree().quit()
+		
+	if(event.is_action_pressed("interact")):
+		interact()
+		
+	if(event.is_action_pressed("dropItem")):
+		dropItem()
 		
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-89), deg_to_rad(89))
 
+func dropItem() -> void:
+	if objectHeld and heldObject:		
+		$Holder.remove_child(heldObject)
+		get_parent().add_child(heldObject) 
+		heldObject.set_position(Vector3(position.x + 1,  position.y,  position.z))
+
+		objectHeld = false
+		heldObject = null
+
+func interact() -> void:
+	var space_state = get_world_3d().direct_space_state
+	var cam = $Head/PlayerCamera
+	var mousepos = get_viewport().get_mouse_position()
+
+	var origin = cam.project_ray_origin(mousepos)
+	var end = origin + cam.project_ray_normal(mousepos) * 20
+	var query = PhysicsRayQueryParameters3D.new()
+	query.from = origin
+	query.to = end
+	query.collide_with_areas = true
+
+	var result = space_state.intersect_ray(query)
+	
+	if result and result.collider:
+		print_debug(result.collider.name)
+		if result.collider.name == "Object":
+			if not objectHeld:
+				heldObject = result.collider
+				result.collider.get_parent().remove_child(heldObject)
+				$Holder.add_child(heldObject)
+				heldObject.set_position(Vector3(0, 0, 0))
+				print_debug(heldObject.position)
+				objectHeld = true
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -29,8 +72,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get input direction and handle movement/deceleration.
 	var input_dir: Vector2 = Input.get_vector("left", "right", "forward", "backward")
 	var direction: Vector3 = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
---
+++ my-game/src/world/object.gd
@@ -0,0 +1,4 @@
+extends StaticBody3D
+
+#func onInput() -> void:
+	
