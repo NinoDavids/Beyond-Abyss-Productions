54c611b6fb9b4952c12d80806e1f7a2053ef5985
NinoDavids <60606129+NinoDavids@users.noreply.github.com>
2024-09-16 10:18:18 +0200

    Added a way to exit the game

--- my-game/src/actors/player/scripts/player.gd
+++ my-game/src/actors/player/scripts/player.gd
@@ -12,6 +12,9 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("quitEditor")):
+		get_tree().quit();
+		
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
