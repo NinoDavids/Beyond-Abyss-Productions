7810226d02ab0b7448822fe7514839b77afda8f4
NinoDavids <nino@caiway.net>
2024-09-20 12:45:08 +0200

    Drop off object

---
+++ my-game/src/world/Dropoff.gd
@@ -0,0 +1,9 @@
+extends Area3D
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.name.contains("Object"):
+		print_debug("dropped off object")
+		
+func _on_body_exited(body: Node3D) -> void:
+	if body.name.contains("Object"):
+		print_debug("picked up object")
