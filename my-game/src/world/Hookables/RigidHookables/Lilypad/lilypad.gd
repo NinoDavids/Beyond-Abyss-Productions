40013549507b96d3aca6c46d63fcf545aeeed982
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2024-10-11 10:25:50 +0200

    make lilypad not rotate

--- my-game/src/world/Hookables/RigidHookables/Lilypad/lilypad.gd
+++ my-game/src/world/Hookables/RigidHookables/Lilypad/lilypad.gd
@@ -12,21 +12,19 @@ var submerged := false
 const water_height := -2.0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	self.lock_rotation = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
+
 func _physics_process(delta: float) -> void:
-	print(water)
 	submerged = false
 	var depth = water.get_height(global_position) - global_position.y
 	if depth > 0:
 		submerged = true
 		apply_central_force(Vector3.UP * float_force * 100 * depth)
-		
+
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if submerged:
 		state.linear_velocity  *= 1 - water_drag
