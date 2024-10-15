77a2716a0efe2a4ec4add78bf12fb90b794955a1
Studeeric <100409570+Studeeric@users.noreply.github.com>
2024-10-15 15:20:37 +0200

    Update AnimationHookable.gd

--- my-game/src/world/Hookables/AnimationHookables/AnimationHookable.gd
+++ my-game/src/world/Hookables/AnimationHookables/AnimationHookable.gd
@@ -4,16 +4,19 @@ class_name AnimationHookable
 @export var animation_player: AnimationPlayer
 @export var animation_name: StringName
 
+var is_finished: bool = false
+
 func _ready() -> void:
 	animation_player.play(animation_name)
 	animation_player.pause()
 
 func reel_in() -> void:
-	animation_player.play(animation_name)
+	if !is_finished:
+		animation_player.play(animation_name)
 
 func pause_animation() -> void:
 	animation_player.pause()
 
 func animation_finished() -> void:
 	EventManager.anim_hookable_finished.emit()
-	queue_free()
+	is_finished = true
