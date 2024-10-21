e230b90ff0a6660b38b663cd32e58ff584289acc
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2024-10-21 15:05:32 +0200

    make panel hittable

---
+++ my-game/src/world/Object/Drain/drain.gd
@@ -0,0 +1,18 @@
+extends StaticBody3D
+
+@export var button: AnimatableBody3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("hit")
+	if body == Bobber:
+		print("hit")
