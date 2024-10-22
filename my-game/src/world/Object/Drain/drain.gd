71d5c2b19031b794be237a04eb4f26c3facf2720
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2024-10-22 14:40:14 +0200

    Make button pushable

--- my-game/src/world/Object/Drain/drain.gd
+++ my-game/src/world/Object/Drain/drain.gd
@@ -1,18 +1,16 @@
 extends StaticBody3D
 
-@export var button: AnimatableBody3D
+@onready var button: AnimatableBody3D = $Button
+@onready var animationPlayer: AnimationPlayer = $Button/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	print("hit")
-	if body == Bobber:
-		print("hit")
+	if body is Bobber:
+		animationPlayer.play('in')
