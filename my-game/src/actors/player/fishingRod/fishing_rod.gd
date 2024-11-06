c617713abcbdc3d1730aef2f2f30dccd2ba5ea7b
Studeeric <21146632@student.hhs.nl>
2024-11-06 16:12:24 +0100

    Charging Fishingrod + responsability

--- my-game/src/actors/player/fishingRod/fishing_rod.gd
+++ my-game/src/actors/player/fishingRod/fishing_rod.gd
@@ -1,7 +1,60 @@
 extends Node3D
 class_name FishingRod
 
-@onready var bobber_mesh: MeshInstance3D = $BobberMesh
+const BOBBER: PackedScene = preload("res://src/actors/player/fishingRod/bobber/Bobber.tscn")
+const SPRING: PackedScene = preload("res://src/actors/player/fishingRod/line/spring.tscn")
+var spring: Node
+
+@export var player: Player
+
+var current_bobber: Bobber
+@onready var bobber_mesh: MeshInstance3D = %BobberMesh
+
+@export var cast_strength: float = 5.5
+@export var strenght_multiplier: float = 0.5
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var is_charging: bool = false
+
+func _ready() -> void:
+	EventManager.anim_hookable_finished.connect(cancel_hook)
+
 
 func set_active(active: bool) -> void:
 	bobber_mesh.visible = !active
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("cast_hook") and !current_bobber:
+		animation_player.play("HoldBack")
+		is_charging = true
+	
+	if event.is_action_released("cast_hook") and !current_bobber and is_charging:
+		animation_player.play("Cast")
+		is_charging = false
+	
+	if event.is_action_pressed("cancel_hook"):
+		animation_player.play("RESET")
+		cancel_hook()
+
+func cancel_hook() -> void:
+	set_active(false)
+	if spring != null:
+		spring.queue_free()
+
+func cast_bobber() -> void:
+	set_active(true)
+	current_bobber = BOBBER.instantiate()
+	current_bobber.player = player
+	get_tree().current_scene.add_child(current_bobber)
+	current_bobber.add_to_group("bobber")
+	current_bobber.global_position = bobber_mesh.global_position ## Get the position of the rod
+	var direction: Vector3 = -player.player_camera.global_transform.basis.z ## Aims in the direction that the camera is pointing
+	direction.y += 1 ## Moves the aim a bit more upwards
+	current_bobber.apply_impulse(direction * cast_strength * strenght_multiplier)
+	create_rope()
+
+func create_rope() -> void:
+	spring = SPRING.instantiate()
+	spring.bobber_mesh = bobber_mesh
+	add_sibling(spring)
+	spring.global_position = current_bobber.global_position
--- my-game/src/actors/player/scripts/player.gd
+++ my-game/src/actors/player/scripts/player.gd
@@ -2,9 +2,6 @@ extends CharacterBody3D
 class_name Player
 
 @onready var head: Node3D = $Head
-const BOBBER: PackedScene = preload("res://src/actors/player/fishingRod/bobber/Bobber.tscn")
-const SPRING: PackedScene = preload("res://src/actors/player/fishingRod/line/spring.tscn")
-var spring: Node
 @onready var player_camera: Camera3D = $Head/PlayerCamera
 @onready var fishing_rod: FishingRod = $Head/FishingRod
 @onready var raycast: RayCast3D = player_camera.get_node("RayCast3D")
@@ -14,14 +11,12 @@ const SPEED: float = 5.0
 const JUMP_VELOCITY: float = 4.5
 
 @export var mouse_sens: float = 0.25
-@export var cast_strength: float = 5.5
 
 var current_bobber: Bobber
 var held_Item: RigidBody3D
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	EventManager.anim_hookable_finished.connect(cancel_hook)
 	EventManager.player_respawned.connect(respawn)
 
 func _input(event: InputEvent) -> void:
@@ -47,32 +42,6 @@ func _input(event: InputEvent) -> void:
 
 	if held_Item:
 		held_Item.global_transform.origin = itemHolder.global_transform.origin
-
-	if event.is_action_pressed("cast_hook") and !current_bobber:
-		cast_bobber()
-
-	if event.is_action_pressed("cancel_hook"):
-		cancel_hook()
-
-func cancel_hook() -> void:
-	fishing_rod.set_active(false)
-	if spring != null:
-		spring.queue_free()
-
-func cast_bobber() -> void:
-	fishing_rod.set_active(true)
-	var clone: Bobber = BOBBER.instantiate()
-	current_bobber = clone
-	current_bobber.player = self
-	get_tree().current_scene.add_child(clone)
-	clone.add_to_group("bobber")
-	clone.global_position = fishing_rod.global_position ## Get the position of the rod
-	var direction: Vector3 = -player_camera.global_transform.basis.z ## Aims in the direction that the camera is pointing
-	direction.y += 1 ## Moves the aim a bit more upwards
-	clone.apply_impulse(direction * cast_strength)
-	spring = SPRING.instantiate()
-	add_sibling(spring)
-	spring.global_position = clone.global_position
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
