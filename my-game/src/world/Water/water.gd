ad7655d405d41594c19ead306bcf070c2eaec878
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2024-11-11 10:25:32 +0100

    add water to env

--- my-game/src/world/Water/water.gd
+++ my-game/src/world/Water/water.gd
@@ -1,4 +1,3 @@
-
 extends Area3D
 
 class_name Water
@@ -18,18 +17,19 @@ func loadWater() -> void:
 	collision_size = Vector3(plane_size.x, 0.1, plane_size.y)
 	water_plane.mesh.size = plane_size
 	collision_shape_3d.shape.size = collision_size
+	print_debug(water_plane.mesh.size.x)
 
 func _ready() -> void:
-	print(water_plane)
 	loadWater()
 
 func _process(_delta: float) -> void:
-	collision_size = Vector3(plane_size.x, 0.1, plane_size.y)
-	if water_plane and collision_shape_3d:
-		if water_plane.mesh.size != plane_size:
-			water_plane.mesh.size = plane_size
-		if collision_shape_3d.shape.size != collision_size:
-			collision_shape_3d.shape.size = collision_size
+	pass
+	#collision_size = Vector3(plane_size.x, 0.1, plane_size.y)
+	#if water_plane and collision_shape_3d:
+		#if water_plane.mesh.size != plane_size:
+		#	water_plane.mesh.size = plane_size
+		#if collision_shape_3d.shape.size != collision_size:
+		#	collision_shape_3d.shape.size = collision_size
 
 func player_hits_water(player: CharacterBody3D) -> void:
 	if player.find_child("Hitbox"):
@@ -39,7 +39,6 @@ func player_hits_water(player: CharacterBody3D) -> void:
 func raise_water(height: float) -> void:
 	var tween := get_tree().create_tween()
 	tween.tween_property(water_plane, "global_position", Vector3(water_plane.global_position.x, height, water_plane.global_position.z), 5)
-
 
 func stop_raise_water(height: float) -> void:
 	var tween := get_tree().create_tween()
