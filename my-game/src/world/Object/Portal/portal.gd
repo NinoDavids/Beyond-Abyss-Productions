5ed2db2daff494450fe7e44b5014d4e540deb6c4
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2024-11-19 15:56:57 +0100

    make new area for the shader to trigger

--- my-game/src/world/Object/Portal/portal.gd
+++ my-game/src/world/Object/Portal/portal.gd
@@ -8,12 +8,15 @@ var used: bool = false
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if !used:
 		out_portal.used = true
-		print_debug(body)
-		if body is Player:
-			print_debug('is_Player')
-			body.teleport_shader.show()
-		
 		body.global_position = out_portal.global_position
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	used = false
+
+func _on_area_3d_2_body_entered(body: Node3D) -> void:
+	if body is Player:
+		body.teleport_shader.show()
+
+func _on_area_3d_2_body_exited(body: Node3D) -> void:
+	if body is Player:
+		body.teleport_shader.hide()
