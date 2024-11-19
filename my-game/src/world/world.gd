747b707d63e1539e09a0e73bda0cd680ff7523e7
Remco <remco.o.2002@outlook.com>
2024-11-19 14:41:07 +0100

    When the fish touch the player the fish damages the player

--- my-game/src/world/world.gd
+++ my-game/src/world/world.gd
@@ -15,8 +15,8 @@ func _ready() -> void:
 	setAchievement("ACH_STARTGAME")
 	get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
 
-func setAchievement(ach):
-	var status = Steam.getAchievement(ach)
+func setAchievement(ach) -> void:
+	var status: Dictionary = Steam.getAchievement(ach)
 	if status["achieved"]:
 		return
 	Steam.setAchievement(ach)
@@ -25,9 +25,9 @@ func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("quitEditor")):
 		get_tree().quit();
 
-func _process(delta):
+func _process(delta: float):
 	time_elapsed += delta
-	var time_to_int = int(time_elapsed)
+	var time_to_int: int = int(time_elapsed)
 
 	if time_to_int >= 3:
 		get_tree().call_group("enemies", "update_target_location", player.global_transform.origin)
