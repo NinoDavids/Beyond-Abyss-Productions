16e6fc06780db7df0bb48dd7f8908d97a3be70e9
Studeeric <21146632@student.hhs.nl>
2024-11-21 14:06:37 +0100

    Animation + Functionality update

--- my-game/src/world/PuzzlePieces/Canons/CanonUp/Model/cannon_up_model.gd
+++ my-game/src/world/PuzzlePieces/Canons/CanonUp/Model/cannon_up_model.gd
@@ -3,5 +3,10 @@ extends Node3D
 
 signal shoot()
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func start_charge_animation() -> void:
+	animation_player.play("ShootUp")
+
 func _shoot() -> void:
 	shoot.emit()
