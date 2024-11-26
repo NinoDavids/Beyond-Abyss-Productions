c0d74c4461c2066136046027954c651c087a8148
NinoDavids <nino@caiway.net>
2024-11-26 14:38:52 +0100

    Projectile goes down

--- my-game/src/world/PuzzlePieces/Canons/Projectile/canon_projectile.gd
+++ my-game/src/world/PuzzlePieces/Canons/Projectile/canon_projectile.gd
@@ -2,17 +2,29 @@ class_name CanonProjectile
 extends CharacterBody3D
 
 @export var speed: float = 1.0
+@export var gravity: float = -9.8
 var direction: Vector3
 var start_position: Vector3
 var end_position: Vector3
+var vertical_velocity: float = 0.0
+
+func _ready() -> void:
+	start_position = global_position
+	end_position = start_position + direction.normalized() * start_position.distance_to(end_position)
 
 func _physics_process(_delta: float) -> void:
 	if direction == null: return
+	
+	vertical_velocity += gravity * _delta
 	
 	if check_distance_reached():
 		queue_free()
 	
 	velocity = direction * speed
+	velocity.y = vertical_velocity
+
+	print_debug(transform)
+
 	look_at(global_position + direction)
 	move_and_slide()
 
