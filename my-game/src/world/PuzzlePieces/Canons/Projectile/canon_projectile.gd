cb7df613b2f092d0dba541c5812dd65b45b78fe5
Studeeric <21146632@student.hhs.nl>
2024-12-04 14:43:04 +0100

    Added puzzle 1-6 and a extra settings to the projectile finder

--- my-game/src/world/PuzzlePieces/Canons/Projectile/canon_projectile.gd
+++ my-game/src/world/PuzzlePieces/Canons/Projectile/canon_projectile.gd
@@ -13,7 +13,7 @@ func _physics_process(_delta: float) -> void:
 		queue_free()
 	
 	velocity = direction * speed
-	look_at(global_position + direction)
+	#look_at(global_position + direction)
 	move_and_slide()
 
 func check_distance_reached() -> bool:
