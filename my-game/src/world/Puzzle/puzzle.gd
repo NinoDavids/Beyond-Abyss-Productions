886ef0da259f10a5307256090445dc9394e5917b
Studeeric <100409570+Studeeric@users.noreply.github.com>
2024-12-05 11:57:08 +0100

    Update puzzle.gd

--- my-game/src/world/Puzzle/puzzle.gd
+++ my-game/src/world/Puzzle/puzzle.gd
@@ -1,21 +1,21 @@
 extends GridMap
 class_name Puzzle
 
-@export var water_sprouts: Array[WaterSpout]
+@export var water_spouts: Array[WaterSpout]
 @export var active: bool= false
 @export var next_puzzle: Puzzle
 
 func _ready() -> void:
-	if water_sprouts.size() > 0 && active:
-		water_sprouts[0].active = true
+	if water_spouts.size() > 0 && active:
+		water_spouts[0].active = true
 
 # Called when the node enters the scene tree for the first time.
-func next_water_sprout() -> void:
-	if water_sprouts.size() > 0:
-		water_sprouts[0].active = false
-		water_sprouts.remove_at(0)
+func next_water_spout() -> void:
+	if water_spouts.size() > 0:
+		water_spouts[0].active = false
+		water_spouts.remove_at(0)
 		
-		if water_sprouts.size() > 0:
-			water_sprouts[0].active = true
-		elif next_puzzle.water_sprouts.size() > 0:
-			next_puzzle.water_sprouts[0].active = true
+		if water_spouts.size() > 0:
+			water_spouts[0].active = true
+		elif next_puzzle.water_spouts.size() > 0:
+			next_puzzle.water_spouts[0].active = true
