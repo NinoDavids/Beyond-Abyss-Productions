3234a67c5ac64284cf1276f5a6fe52f359222b3d
Studeeric <21146632@student.hhs.nl>
2024-12-07 16:00:12 +0100

    Updated main menu

--- my-game/src/util/SettingsManager/SettingsManager.gd
+++ my-game/src/util/SettingsManager/SettingsManager.gd
@@ -12,9 +12,21 @@ func get_inverted_y_float() -> float:
 
 func get_masterVolume() -> float:
 	return settings.masterVolume
-	
+
 func get_musicVolume() -> float:
 	return settings.musicVolume
-	
+
 func get_SFXVolume() -> float:
 	return settings.SFXVolume
+
+func set_masterVolume(value: float) -> void:
+	settings.masterVolume = value
+	AudioServer.set_bus_volume_db(0, settings.masterVolume)
+
+func set_musicVolume(value: float) -> void:
+	settings.musicVolume = value
+	AudioServer.set_bus_volume_db(1, settings.musicVolume)
+
+func set_SFXVolume(value: float) -> void:
+	settings.SFXVolume = value
+	AudioServer.set_bus_volume_db(2, settings.SFXVolume)
