00c336d9bff73f743cff3723406c5f3162f51cf8
Studeeric <21146632@student.hhs.nl>
2024-12-07 16:16:48 +0100

    Revert "Revert "Updated main menu""
    This reverts commit 7fb4f1f55e9bc8edf3d6010013bb8ecf0d4a00ab.

--- my-game/src/UI/MainMenu/Scripts/options_menu.gd
+++ my-game/src/UI/MainMenu/Scripts/options_menu.gd
@@ -28,15 +28,15 @@ func _on_invert_button_toggled(toggled_on: bool) -> void:
 	ResourceSaver.save(SettingsManager.settings)
 
 func _on_master_slider_drag_ended(_value_changed: bool) -> void:
-	SettingsManager.settings.masterVolume = masterVolume.value
+	SettingsManager.set_masterVolume(masterVolume.value)
 	ResourceSaver.save(SettingsManager.settings)
 	
 func _on_music_slider_drag_ended(_value_changed: bool) -> void:
-	SettingsManager.settings.musicVolume = musicVolume.value
+	SettingsManager.set_musicVolume(musicVolume.value)
 	ResourceSaver.save(SettingsManager.settings)
 	
 func _on_sfx_slider_drag_ended(_value_changed: bool) -> void:
-	SettingsManager.settings.SFXVolume = SFXVolume.value
+	SettingsManager.set_SFXVolume(SFXVolume.value) 
 	ResourceSaver.save(SettingsManager.settings)
 
 
