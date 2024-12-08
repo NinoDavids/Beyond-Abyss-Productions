d3319af69444d73fc9ff78500d4af2a857b53bca
Studeeric <21146632@student.hhs.nl>
2024-12-08 01:31:41 +0100

    Added cringe checkpoints vgm speler layer goedgezet enzo

--- my-game/src/world/Puzzle/puzzle.gd
+++ my-game/src/world/Puzzle/puzzle.gd
@@ -1,13 +1,21 @@
 extends GridMap
 class_name Puzzle
 
+@onready var checkpoint: Checkpoint = $Checkpoint
+
 @export var water_spouts: Array[WaterSpout]
-@export var active: bool= false
+@export var active: bool = false:
+	set(value):
+		active = value
+		if checkpoint:
+			checkpoint.is_active = value
 @export var next_puzzle: Puzzle
 
 func _ready() -> void:
 	if water_spouts.size() > 0 && active:
 		water_spouts[0].active = true
+	
+	checkpoint.is_active = active
 
 # Called when the node enters the scene tree for the first time.
 func next_water_spout() -> void:
@@ -18,4 +26,5 @@ func next_water_spout() -> void:
 		if water_spouts.size() > 0:
 			water_spouts[0].active = true
 		elif next_puzzle.water_spouts.size() > 0:
+			next_puzzle.active = true
 			next_puzzle.water_spouts[0].active = true
