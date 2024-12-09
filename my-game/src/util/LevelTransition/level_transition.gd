a65fae0d436acd4961d5c301d125d84e8f41fe3e
Studeeric <21146632@student.hhs.nl>
2024-12-09 11:43:39 +0100

    Fixed loop

--- my-game/src/util/LevelTransition/level_transition.gd
+++ my-game/src/util/LevelTransition/level_transition.gd
@@ -3,5 +3,5 @@ extends Area3D
 @export var next_level: PackedScene
 
 func _on_body_entered(body: Node3D) -> void:
-	if body is Player:
+	if body is Player and next_level != null:
 		get_tree().change_scene_to_packed(next_level)
