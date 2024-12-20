6cd2c8bfcc0bcc5a2fa33aabb7cb1ac2caefd3b9
RoyDuijndam1 <Roy.Duijndam1@gmail.com>
2024-12-20 12:02:22 +0100

    remove portal after projectile hit
    Add animationPlayer to portal and reduce scale of shader when the portal is hit with the projectile. After animation is finished remove the shader

--- my-game/src/world/Object/Portal/portal_shader.gd
+++ my-game/src/world/Object/Portal/portal_shader.gd
@@ -7,6 +7,7 @@ var is_front_direction: bool:
 
 @onready var shader: MeshInstance3D = $PortalShader
 @onready var material: ShaderMaterial = shader.get_surface_override_material(0)
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
 	load_portal()
@@ -16,3 +17,11 @@ func load_portal() -> void:
 		material.set_shader_parameter("PORTAL_NORMAL", Vector3(0,0,1))
 	else:
 		material.set_shader_parameter("PORTAL_NORMAL", Vector3(1,0,0))
+
+func close_portal() -> void:
+	animation_player.play("close_portal")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "close_portal":
+		self.queue_free()
